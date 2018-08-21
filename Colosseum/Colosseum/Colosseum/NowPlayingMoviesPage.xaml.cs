0e2264fed3dff8901c8bd17ccd334e3533777608
DESKTOP-H1OTVQ9\Dickenz <dickienhevs@gmail.com>
2018-08-21 09:01:20 +0200

    add vs project

---
+++ Colosseum/Colosseum/Colosseum/NowPlayingMoviesPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Colosseum.Models;
+using Colosseum.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Colosseum
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NowPlayingMoviesPage : ContentPage
+    {
+        public ObservableCollection<NowPlayingMovie> NowPlayingMovies;
+
+        public NowPlayingMoviesPage()
+        {
+            InitializeComponent();
+            NowPlayingMovies = new ObservableCollection<NowPlayingMovie>();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            ApiServices apiServices = new ApiServices();
+            var nowPlayingMovies = await apiServices.GetNowPlayingMovies();
+            foreach (var nowPlayingMovie in nowPlayingMovies)
+            {
+                NowPlayingMovies.Add(nowPlayingMovie);
+            }
+            LvNowPlaying.ItemsSource = NowPlayingMovies;
+        }
+    }
+}
---
+++ Colosseum/Colosseum/Colosseum/Services/ApiServices.cs
@@ -0,0 +1,48 @@
+﻿using Colosseum.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colosseum.Services
+{
+    public class ApiServices
+    {
+        private readonly string nowPlayingMoviesUrl = @"http://colosseum.somee.com/api/NowPlayingMovies";
+        private readonly string upComingMoviesUrl = @" http://colosseum.somee.com/api/UpComingMovies";
+        private readonly string orderApiUrl = @"http://colosseum.somee.com/api/Orders";
+
+        public async Task<List<NowPlayingMovie>> GetNowPlayingMovies()
+        {
+            var client = new HttpClient();
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, nowPlayingMoviesUrl);
+            requestMessage.Headers.Add("ApiKey", "fd78d5db-fe18-43d6-b690-d582373b9a1c");
+            var responseMessage = await client.SendAsync(requestMessage);
+            var movieResponse = await requestMessage.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<NowPlayingMovie>>(movieResponse);
+        }
+
+        public async Task<List<UpComingMovie>> GetUpComingMovies()
+        {
+            var client = new HttpClient();
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, upComingMoviesUrl);
+            requestMessage.Headers.Add("ApiKey", "fd78d5db-fe18-43d6-b690-d582373b9a1c");
+            var responseMessage = await client.SendAsync(requestMessage);
+            var movieResponse = await requestMessage.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<UpComingMovie>>(movieResponse);
+        }
+
+        public async Task<bool> Order(BookTicket bookTicket)
+        {
+            var client = new HttpClient();
+            var json = JsonConvert.SerializeObject(bookTicket);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            content.Headers.Add("ApiKey", "fd78d5db-fe18-43d6-b690-d582373b9a1c");
+            var response = await client.PostAsync(orderApiUrl, content);
+
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
